f9bf23db95a43d673d3cd5ddd1e9f2edba0e6047
Paul Granados <Paul Granados@DESKTOP-J4RED30>
2025-08-07 22:13:25 -0600

    "cambios 07-08-2025"

---
+++ src/numeross/Numeross.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numeross;
+import java.util.Scanner;
+/**
+ *
+ * @author Paul Granados
+ */
+public class Numeross {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número ");
+        int numero = sc.nextInt();
+        if (numero < 0) {
+            System.out.println("Error: el número debe ser positivo");
+        } else if (numero < 10) {
+            System.out.println("El numero tiene 1 cifra");
+        } else if (numero < 100) {
+            System.out.println(" El numero tiene 2 cifras");
+        } else if (numero < 1000) {
+            System.out.println(" El numero tiene 3 cifras");
+        } else {
+            System.out.println("Error: el número tiene más de 3 cifras");
+        }
+    }
+}
+
+
+    
+
